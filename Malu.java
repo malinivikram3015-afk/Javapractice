1b8759c6804f951109191d689f7cf128b213490e
malinivikram3015-afk <malinivikram3015@gmail.com>
2025-11-25 21:57:17 +0530

    Add main method and user input for feature toggle
    Fix typo in main method declaration and add user input functionality.

---
+++ Malu.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Malu{
+public sattic void main(String[] args){
+Scanner malu = new Scanner(System.in);
+System.out.println("Enable a certain features (true /false)");
+Boolean track = malu.nextBoolean();
+System.out.println(track);
+}
+}
